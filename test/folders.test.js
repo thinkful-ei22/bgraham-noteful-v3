b386674957a9e2514eebd5941d8ce346fd310f46
Brandon Graham <bgraham452@gmail.com>
2018-07-12 14:26:34 -0400

    created folder.test file

---
+++ test/folders.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require ('chai-http');
+
+const mongoose = require ('mongoose');
+
+const app = require ('../server');
+
+const { TEST_MONGODB_URI } = require ('../config');
+
+const Folder = require('../models/folders');
+const seedFolders = require('../db/seed/folders');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
